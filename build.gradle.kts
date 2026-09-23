481b67334561c818f5a56351accb86ebb24e5c3b
mboya <mboyaberry@gmail.com>
2026-09-23 11:30:08 +0300

    Declare Firebase google-services and Crashlytics Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
     id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
